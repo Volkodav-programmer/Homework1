57a4ac1694a79c64b169681836f85a82165cb515
Volkodav-programmer <Zybenkos2016@gmail.com>
2020-09-20 16:34:14 +0300

    Работает!

--- Tv.py
+++ Tv.py
@@ -1,30 +1,32 @@
 class Tv:
 
-    def __init__(self, channel, volume):
+    def __init__(self, channel=0, volume=0):
         self.channel=channel
         self.volume=volume
 
-    def set_volume(self, volume):
-        volume=int(input('Введите какую громкость вы хотите поставить'))
+    def set_volume(self, volume=0):
+        volume=int(input('Введите какую громкость вы хотите поставить: '))
         self.volume=volume
-        if volume>100:
-            volume=100
-        elif volume<0:
-            volume=0
+        if self.volume>100:
+            print('Ишь чего захотел')
+            self.volume=100
+        elif self.volume<0:
+            print('Ишь чего захотел')
+            self.volume=0
 
 
-    def set_channel(self, channel):
-        channel=int(input('Введите какой канал вы хотите включить'))
+    def set_channel(self, channel=0):
+        channel=int(input('Введите какой канал вы хотите включить: '))
         self.channel=channel
-        if channel>225:
+        if self.channel>225:
             print('Такого канала не существует')
-            channel=0
-        elif channel<0:
+            self.channel=0
+        elif self.channel<0:
             print('Такого канала не существует')
-            channel=0
+            self.channel=0
     
     def talk(self):
-        print('Вы смотрите', self.channel, 'на громкости', self.volume)
+        print('Вы смотрите', self.channel, 'канал, на громкости', self.volume)
 
 def main():
     tv=Tv()
@@ -32,12 +34,13 @@ def main():
     while choice!='0':
         print \
         ('''
-        Меню
+        *Шумит по телевизорски*
+                Меню
 
         0-выключить телевизор
         1-изменить громкость
         2-изменить канал
-
+        3-просмотреть параметры
         ''')
         choice=input('Ваш выбор: ')
         print()
@@ -50,12 +53,16 @@ def main():
         #Изменение звука
 
         elif choice=='1':
-            tv.set_volume
+            tv.set_volume()
 
         #Изменение канала
            
         elif choice=='2':
-            tv.set_channel
+            tv.set_channel()
+        
+        #Просмотр параметров
+        elif choice=='3':
+            tv.talk()
 
         else:
             print('Ты чё, выбери пункт из меню', choice)
